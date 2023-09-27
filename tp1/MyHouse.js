bd220a8d430a158d3c9d72f998abe32cc848ac40
inescardos <ines.caardoso@gmail.com>
2023-09-27 17:59:18 +0100

    Refactored house class and changed colors

--- tp1/MyHouse.js
+++ tp1/MyHouse.js
@@ -5,7 +5,6 @@ import { MyApp } from './MyApp.js';
  * This class contains a Floor representation
  */
 class MyHouse extends THREE.Object3D {
-
     /**
      * 
      * @param {MyApp} app the application object
@@ -14,42 +13,50 @@ class MyHouse extends THREE.Object3D {
         super();
         this.app = app;
         this.type = 'Group';
+        let size = 20;
 
-        // Create a Plane Mesh with basic material
-        //floor
-        let plane = new THREE.PlaneGeometry( 10, 10 );
-        const floor = new THREE.Mesh( plane, app.planeMaterial );
-        floor.rotation.x = -Math.PI / 2;
-        floor.position.y = -0;
-        this.add(floor);
+        // Define the plane geometry at the class level
+        this.planeGeometry = new THREE.PlaneGeometry(size * 2, size * 2);
+        this.createFloor(size);
+        this.createWalls(size);
+    }
 
+    createWalls(size) {
+        const material = new THREE.MeshBasicMaterial({ color: 0xF5F5DD });
 
-        //walls
-        const wall1 = new THREE.Mesh( plane, app.planeMaterial );
+        const wall1 = new THREE.Mesh(this.planeGeometry, material);
         wall1.rotation.y = -Math.PI / 2;
-        wall1.position.x = 5;
-        wall1.position.y = 5;
+        wall1.position.x = size;
+        wall1.position.y = size;
         this.add(wall1);
 
-        const wall2 = new THREE.Mesh( plane, app.planeMaterial );
+        const wall2 = new THREE.Mesh(this.planeGeometry, material);
         wall2.rotation.y = Math.PI / 2;
-        wall2.position.x = -5;
-        wall2.position.y = 5;
+        wall2.position.x = -size;
+        wall2.position.y = size;
         this.add(wall2);
 
-        const wall3 = new THREE.Mesh( plane, app.planeMaterial );
+        const wall3 = new THREE.Mesh(this.planeGeometry, material);
         wall3.rotation.y = -Math.PI;
-        wall3.position.z = 5;
-        wall3.position.y = 5;
+        wall3.position.z = size;
+        wall3.position.y = size;
         this.add(wall3);
 
-        const wall4 = new THREE.Mesh( plane, app.planeMaterial );
+        const wall4 = new THREE.Mesh(this.planeGeometry, material);
         wall4.rotation.y = 0;
-        wall4.position.z = -5;
-        wall4.position.y = 5;
+        wall4.position.z = -size;
+        wall4.position.y = size;
         this.add(wall4);
+    }
 
-        
+    createFloor(size) {
+    const material = new THREE.MeshBasicMaterial({ color: 0xD2BD96 /*C8AE7E*/ });
+        const floor = new THREE.Mesh(this.planeGeometry, material);
+
+        floor.rotation.x = -Math.PI / 2;
+        floor.position.y = -0;
+
+        this.add(floor);
     }
 }
 
