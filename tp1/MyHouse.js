e344ca3ca1fb4bfe0954f6d3470bca94a922391e
Joana Santos <up202006279@edu.fe.up.pt>
2023-10-13 21:49:18 +0100

    changed wallPaper

--- tp1/MyHouse.js
+++ tp1/MyHouse.js
@@ -31,7 +31,7 @@ class MyHouse extends THREE.Object3D {
         ];
 
         const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
-        const textureWallPaper = new THREE.TextureLoader().load('textures/wallPaper2.jpg');
+        const textureWallPaper = new THREE.TextureLoader().load('textures/wallPaper1.jpg');
         textureWallPaper.wrapS = THREE.RepeatWrapping;
         textureWallPaper.wrapT = THREE.RepeatWrapping;
         textureWallPaper.rotation = this.rotate
