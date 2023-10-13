4d8cd8c5bd22bfa804a0a06f21479642aee267a2
inescardos <ines.caardoso@gmail.com>
2023-10-13 11:06:10 +0100

    Created a book class

---
+++ tp1/MyBook.js
@@ -0,0 +1,98 @@
+import * as THREE from 'three';
+import { MyApp } from './MyApp.js';
+
+/**
+ * This class contains a carpet representation
+ */
+export class MyBook extends THREE.Object3D {
+    /**
+     * 
+     * @param {MyApp} app the application object
+     * @param {number} width
+     * @param {number} length
+     * @param {number} height
+     * @param {string} color
+     * @param {number} numBooks
+     * @param {number[]} position - An array with x, y, z coordinates
+     */
+    constructor(app, width, length, height, numBooks, position) {
+        super();
+        this.type = 'Group';
+        this.app = app;
+        this.width = width;
+        this.length = length;
+        this.height = height;
+        this.numBooks = numBooks
+        this.position.set(position[0], position[1], position[2]);
+        this.coverThickness = 0.01;
+
+        this.createBookPile(position);
+    }
+
+    createBook(position, color) {
+        this.position.set(position[0], position[1], position[2]);
+
+        // Pages
+        this.pagesTexture = new THREE.TextureLoader().load("textures/pages.jpg");
+        this.diffusePlaneColor = "#FFFFFF";
+        this.specularPlaneColor = "#777777";
+        this.planeShininess = 25;
+        const materialPages = new THREE.MeshPhongMaterial({ color: this.diffusePlaneColor, 
+            specular: this.specularPlaneColor, emissive: "#000000", shininess: this.planeShininess, map: this.pagesTexture });
+
+        const geometryPages = new THREE.BoxGeometry(this.length, this.height, this.width);
+        const pages = new THREE.Mesh(geometryPages, materialPages);
+        pages.position.set(position[0], position[1], position[2]);
+
+        // Cover
+
+        // front and back cover
+        const geometryCover = new THREE.BoxGeometry(this.length, this.coverThickness, this.width);
+        const materialCover = new THREE.MeshPhongMaterial({ color: color });
+        
+        const frontCover = new THREE.Mesh(geometryCover, materialCover);
+        frontCover.position.set(position[0], position[1] + this.height / 2 + this.coverThickness / 2, position[2]);
+        const backCover = new THREE.Mesh(geometryCover, materialCover);
+        backCover.position.set(position[0], position[1] - this.height / 2 - this.coverThickness / 2, position[2]);
+
+        // spine
+        const geometrySpine = new THREE.BoxGeometry(this.length, this.coverThickness, this.height);
+        const spine = new THREE.Mesh(geometrySpine, materialCover);
+        spine.rotateX(Math.PI / 2);
+        spine.position.set(position[0], position[1], position[2] - this.width / 2);
+        
+        // Create a group to contain the book components
+        const book = new THREE.Group(); 
+        book.add(pages, frontCover, backCover, spine);
+        book.position.set(position[0], position[1], position[2]);
+        this.add(book);
+        
+    }
+
+    getRandomColor() {
+        const colors =   ['#FFC0CB', '#AEC6CF', '#B2FF66', '#FFF68F', '#B19CD9', '#FFD700'];
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        const randomColor = colors[randomIndex];
+
+        const threeColor = new THREE.Color(randomColor);
+        return threeColor;
+    }
+
+    createBookPile(startPosition) {
+        const spacing = this.height/2 + 0.01;
+    
+        for (let i = 0; i < this.numBooks; i++) {
+            const position = [
+                startPosition[0], 
+                startPosition[1] + i * spacing, 
+                startPosition[2] 
+            ];
+    
+            const color = this.getRandomColor();
+            this.createBook(position, color); 
+        }
+    }
+    
+}
+
+MyBook.prototype.isGroup = true;
