aebf2df761452cc4804f971eae8397a77abe10f1
inescardos <ines.caardoso@gmail.com>
2023-10-17 19:38:08 +0100

    Changed flower structure

--- tp1/MyFlower.js
+++ tp1/MyFlower.js
@@ -9,12 +9,6 @@ class MyFlower extends THREE.Object3D {
 
     /**
      * 
-     * @param {MyApp} app the application object
-     * @param {number} size the size of each axis 
-     * @param {hex} color the color of the flower
-     * @param {list} position the position of the flower
-     * @param {number} angle the angle of the flower in the y axis
-     * @param {boolean} shadow if the object has shadow or not
      */
     constructor(app, size, color, position, angle, shadow=false) {
         super();
@@ -27,6 +21,9 @@ class MyFlower extends THREE.Object3D {
         this.samplesV = 20; // maximum defined in MyGuiInterface
         this.builder = new MyNurbsBuilder();
 
+        this.petalTexture = new THREE.TextureLoader().load("textures/petal.jpg");
+        this.petalTexture.repeat.set(5, 5);
+        this.materialPetal = new THREE.MeshPhongMaterial({shininess: 50,  map: this.petalTexture, side: THREE.DoubleSide });
 
         this.createFLower();
         this.position.set(position[0], position[1], position[2]);
@@ -35,70 +32,101 @@ class MyFlower extends THREE.Object3D {
     }
 
     createFLower() {
-        // Stem of the flower
-        const material = new THREE.MeshPhongMaterial({ color: 0xbdecb6 });
-        const stem = new THREE.CatmullRomCurve3([
-            new THREE.Vector3(0, -1, 0),
-            new THREE.Vector3(0, 0, 0),
-            new THREE.Vector3(0, 0.3, 0),
-            new THREE.Vector3(-0.1, 0.5, 0),
-            new THREE.Vector3(-0.15, 0.5, 0),
-            new THREE.Vector3(-0.2, 0.4, 0),
-            new THREE.Vector3(-0.1, 0.3, 0),
-            new THREE.Vector3(0, 0.3, 0),
-            new THREE.Vector3(0, 0.7, 0),
-            new THREE.Vector3(0.1, 0.9, 0),
-            new THREE.Vector3(0.15, 0.9, 0),
-            new THREE.Vector3(0.2, 0.8, 0),
-            new THREE.Vector3(0.1, 0.7, 0),
-            new THREE.Vector3(0, 1.2, 0)
-        ]);
-        const tubeRadius = 0.01; // Adjust this value to control the stem's radius
-        const tubeSegments = 100; // Adjust this value to control the smoothness of the tube
-
-        const geometryStem = new THREE.TubeGeometry(stem, tubeSegments, tubeRadius, 8, false);
-        const stemObject = new THREE.Mesh(geometryStem, material);
-        if (this.shadow) {
-            stemObject.castShadow = true;
-        }
-        this.add(stemObject);
+       
         // Center of the flower
-        const flowerCenterGeometry = new THREE.CircleGeometry(0.06, 32);
-        const flowerCenterMaterial = new THREE.MeshPhongMaterial({ color: 0xffff66, side: THREE.DoubleSide });
+        const flowerCenterShape = new THREE.Shape();
+        flowerCenterShape.moveTo(0, -0.1); // Move to the starting point
+        flowerCenterShape.absarc(0, 0, 0.2, 0, Math.PI * 2, false); // Create a filled circle shape
+
+        // Extrude the shape to give it thickness
+        const extrusionSettings = {
+            steps: 1, // Number of depth layers
+            depth: 0.01, // Adjust the thickness here
+            bevelEnabled: false, // No bevel
+        };
+
+        const flowerCenterGeometry = new THREE.ExtrudeGeometry(flowerCenterShape, extrusionSettings);
+        const flowerCenterMaterial = new THREE.MeshPhongMaterial({ color: 0x513A26, side: THREE.DoubleSide });
         const flowerCenterMesh = new THREE.Mesh(flowerCenterGeometry, flowerCenterMaterial);
-        if(this.shadow){
+        if (this.shadow) {
             flowerCenterMesh.castShadow = true;
         }
         flowerCenterMesh.position.set(0, 1.3, 0);
         this.add(flowerCenterMesh);
 
         // Petals
-        const numPetals = 8; // Adjust the number of petals as needed
+        const numPetals = 17; // Adjust the number of petals as needed
         const petalAngleIncrement = (Math.PI * 2) / numPetals;
 
         for (let i = 0; i < numPetals; i++) {
-            const petalGeometry = new THREE.ConeGeometry(0.04, 0.1, 32);
-            const petalMaterial = new THREE.MeshPhongMaterial({ color: this.color });
-            const petalMesh = new THREE.Mesh(petalGeometry, petalMaterial);
-            if(this.shadow){
+            // Create an elliptical petal using a custom geometry
+            const petalGeometry = new THREE.ShapeGeometry(this.createPetalShape());
+            const petalMesh = new THREE.Mesh(petalGeometry, this.materialPetal);
+            if (this.shadow) {
                 petalMesh.castShadow = true;
             }
 
             // Position each petal around the flower center
             const angle = i * petalAngleIncrement;
-            const radius = 0.1; // Adjust the radius as needed
+            const radius = 0.29; // Adjust the radius as needed
             const x = radius * Math.cos(angle);
             const z = 0;
-            const y = 1.3 + radius * Math.sin(angle);; // Adjust the height of the petals as needed
+            const y = 1.3 + radius * Math.sin(angle); // Adjust the height of the petals as needed
             petalMesh.position.set(x, y, z);
 
             // Rotate petals to face outward
             petalMesh.rotation.z = angle - Math.PI / 2;
 
-
             this.add(petalMesh);
+
+
+            const coneGeometry = new THREE.ConeGeometry(radius, 0.15, 32);
+            const coneMaterial = new THREE.MeshPhongMaterial({ color: 0xbdecb6 });
+            const coneMesh = new THREE.Mesh(coneGeometry, coneMaterial);
+            coneMesh.rotateY()
+        
+            // Position the cone
+            coneMesh.position.set(0, 1.3, 0); // Adjust the position as needed
+            if (this.shadow) {
+                coneMesh.castShadow = true;
+            }
+        
+            this.add(coneMesh);
         }
 
+         // Back of the flower
+         const material = new THREE.MeshPhongMaterial({ color: 0xbdecb6 });
+         let backHeight = 0.07; 
+         const flowerBackGeometry = new THREE.ConeGeometry(0.22, backHeight, 32);
+         const flowerBackMesh = new THREE.Mesh(flowerBackGeometry, material);
+         flowerBackMesh.rotateX(-Math.PI/2);
+         // Position the cone
+         flowerBackMesh.position.set(0, 1.3, -backHeight/2-0.005); // Adjust the position as needed
+ 
+         this.add(flowerBackMesh);
+         // Stem of the flower
+         const stem = new THREE.CatmullRomCurve3([
+             new THREE.Vector3(0, 0, 0),
+             new THREE.Vector3(0, 0.7, 0),
+             new THREE.Vector3(0, 1, -0.2),
+             new THREE.Vector3(0, 1.2, -backHeight-0.1),
+             new THREE.Vector3(0, 1.3, -backHeight+0.01)
+         ]);
+         const tubeRadius = 0.01; // Adjust this value to control the stem's radius
+         const tubeSegments = 100; // Adjust this value to control the smoothness of the tube
+ 
+         const geometryStem = new THREE.TubeGeometry(stem, tubeSegments, tubeRadius, 8, false);
+         const stemObject = new THREE.Mesh(geometryStem, material);
+         if (this.shadow) {
+             stemObject.castShadow = true;
+         }
+         this.add(stemObject);
+    }
+
+    createPetalShape() {
+        const petalShape = new THREE.Shape();
+        petalShape.ellipse(0, 0, 0.06, 0.12, 0, Math.PI * 2, false, 0);
+        return petalShape;
     }
 
 }
