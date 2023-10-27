1ca94969c6b67aa53b50e7f208892adfe6553711
inescardos <ines.caardoso@gmail.com>
2023-10-27 12:03:54 +0100

    Updated the Material class

--- tp2/MyMaterial.js
+++ tp2/MyMaterial.js
@@ -8,18 +8,29 @@ class MyMaterial extends THREE.MeshPhongMaterial{
 
     constructor(materialData, texture ) {
         super();
-        this.specular = materialData.color;
-        this.shininess = materialData.shininess;
-        this.emissive  = materialData.emissive;
-        this.wireframe = materialData.wireframe;
         this.texture = texture;
+        this.side = (materialData.twosided)? THREE.DoubleSide: THREE.FrontSide;
+        this.flatShading = (materialData.shading == "flat")? true : false;
+
+
+        this.material = new THREE.MeshPhongMaterial({
+            specular: materialData.color,
+            shininess: materialData.shininess,
+            emissive: materialData.emissive,
+            wireframe: materialData.wireframe,
+            map: this.texture,
+            flatShading: this.flatShading
+        });
+
     }
 
-    updateMaterial(materialData){
+    updateMaterial(materialData, texture){
         this.specular = materialData.color;
         this.shininess = materialData.shininess;
         this.emissive  = materialData.emissive;
-        this.wireframe = materialData.wireframe;          
+        this.wireframe = materialData.wireframe;     
+        this.texture = texture;
+        this.side = (materialData.twosided)? THREE.DoubleSide: THREE.FrontSide;     
     }
 
 }
