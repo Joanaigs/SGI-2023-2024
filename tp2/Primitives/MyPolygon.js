d380bf89ec2c53ebf8de5ffa92ad0ef05441ed8f
Joana Santos <up202006279@edu.fe.up.pt>
2023-11-19 18:21:03 +0000

    fixed polygon

--- tp2/Primitives/MyPolygon.js
+++ tp2/Primitives/MyPolygon.js
@@ -8,132 +8,103 @@ class MyPolygon extends THREE.Group {
     this.stacks = primitiveData.stacks;
     this.slices = primitiveData.slices;
     this.color_c = primitiveData.color_c;
-    this.color_p =primitiveData.color_p;
+    this.color_p = primitiveData.color_p;
 
     this.createPolygon();
   }
 
   createPolygon() {
-    this.geometry = new THREE.BufferGeometry();
-    const vertices = [];
-    const normals = [];
-    const colors = [];
-    const part = this.radius/this.stacks;
+    const interpolate = (startValue, endValue, percentage) =>
+    startValue + percentage * (endValue - startValue);
+    const interpolateColor = (startColor, endColor, percentage) => [
+      interpolate(startColor.r, endColor.r, percentage),
+      interpolate(startColor.g, endColor.g, percentage),
+      interpolate(startColor.b, endColor.b, percentage),
+  ];
 
-    // Calculate vertices and colors
-    for (let i = 0; i < this.stacks; i++) {
+    this.geometry = new THREE.BufferGeometry();
+    const vertices = [0, 0, 0];
+    const normals = [0, 0, 1];
+    const colors = [this.color_c.r, this.color_c.g, this.color_c.b];
+    const indices = [];
+    const uv = [0.5, 0.5];
+    const part = this.radius / this.stacks;
+    const uvCenterU = 0.5;
+    const uvCenterV = 0.5;
+    const uvRadius = 0.5;
+
+    let stackPercentage = 1 / this.stacks;
+    let stackColor = interpolateColor(this.color_c, this.color_p, stackPercentage);
+
+    for (let currentSlice = 0; currentSlice < this.slices; currentSlice++) {
+        const angle = (currentSlice / this.slices) * (2 * Math.PI);
+        const x = part * Math.cos(angle);
+        const y = part * Math.sin(angle);
+        vertices.push(x, y, 0);
+        normals.push(0, 0, 1);
+        const currentSliceIdx = 1 + currentSlice % this.slices;
+        const nextSliceIdx = 1 + (currentSlice + 1) % this.slices;
+        indices.push(0, currentSliceIdx, nextSliceIdx);
+        colors.push(...stackColor);
+        uv.push(
+          uvCenterU + uvRadius * stackPercentage * Math.cos(angle),
+          uvCenterV + uvRadius * stackPercentage * Math.sin(angle)
+        );
+    }
+
+    for (let i = 1; i < this.stacks; i++) {
+      const currentRadius = (i + 1) * part;
+      stackPercentage = (1 + i) / this.stacks;
+      let stackColor = interpolateColor(this.color_c, this.color_p, stackPercentage);
       for (let j = 0; j < this.slices; j++) {
-        const theta1 = (i / this.stacks) * Math.PI;
-        const phi1 = (j / this.slices) * 2 * Math.PI;
-    
-        const theta2 = ((i + 1) / this.stacks) * Math.PI;
-        const phi2 = ((j + 1) / this.slices) * 2 * Math.PI;
-    
-        const x1 = (part*i) * Math.sin(theta1) * Math.cos(phi1);
-        const y1 = (part*i) * Math.sin(theta1) * Math.sin(phi1);
-        const z1 = 0;
-    
-        const x2 = (part*i) * Math.sin(theta1) * Math.cos(phi2);
-        const y2 = (part*i) * Math.sin(theta1) * Math.sin(phi2);
-        const z2 = 0;
-    
-        const x3 = (part*i) * Math.sin(theta2) * Math.cos(phi1);
-        const y3 = (part*i) * Math.sin(theta2) * Math.sin(phi1);
-        const z3 = 0;
-    
-        const normal1 = new THREE.Vector3(x1, y1, z1).normalize();
-        const normal2 = new THREE.Vector3(x2, y2, z2).normalize();
-        const normal3 = new THREE.Vector3(x3, y3, z3).normalize();
-    
-        vertices.push(x1, y1, z1, x2, y2, z2, x3, y3, z3);
-        normals.push(normal1.x, normal1.y, normal1.z, normal2.x, normal2.y, normal2.z, normal3.x, normal3.y, normal3.z);
-    
-        // Interpolate color radially from center to periphery
-        const radialInterpolation = Math.sqrt(x1**2 + y1**2) / (part*i);
-        const color1 = new THREE.Color().lerpColors(this.color_c, this.color_p, radialInterpolation);
-    
-        // Repeat the same for color2 and color3
-        const radialInterpolation2 = Math.sqrt(x2**2 + y2**2) / (part*i);
-        const color2 = new THREE.Color().lerpColors(this.color_c, this.color_p, radialInterpolation2);
-    
-        const radialInterpolation3 = Math.sqrt(x3**2 + y3**2) / (part*i);
-        const color3 = new THREE.Color().lerpColors(this.color_c, this.color_p, radialInterpolation3);
-    
-        colors.push(color1.r, color1.g, color1.b, color2.r, color2.g, color2.b, color3.r, color3.g, color3.b);
+        const angle = (j / this.slices) * (2 * Math.PI);
+        const x = currentRadius * Math.cos(angle);
+        const y = currentRadius * Math.sin(angle);
+        vertices.push(x, y, 0);
+        normals.push(0, 0, 1);
+        colors.push(...stackColor);
+
+        const currentSlicePreviousStackIdx =
+          1 + (i - 1) * this.slices + j % this.slices;
+        const nextSlicePreviousStackIdx =
+          1 + (i - 1) * this.slices + (j + 1) % this.slices;
+
+        const currentSliceIdx = 1 + i * this.slices + j % this.slices;
+        const nextSliceIdx = 1 + i * this.slices + (j + 1) % this.slices;
+
+        indices.push(
+          currentSliceIdx,
+          nextSliceIdx,
+          nextSlicePreviousStackIdx
+        );
+        indices.push(
+          currentSliceIdx,
+          nextSlicePreviousStackIdx,
+          currentSlicePreviousStackIdx
+        );
+        uv.push(
+          uvCenterU + uvRadius * stackPercentage * Math.cos(angle),
+          uvCenterV + uvRadius * stackPercentage * Math.sin(angle)
+        );
       }
     }
+
     console.log(vertices);
+    this.geometry.setIndex(indices);
     this.geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
     this.geometry.setAttribute('normal', new THREE.Float32BufferAttribute(normals, 3));
     this.geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+    this.geometry.setAttribute('uv', new THREE.Float32BufferAttribute(uv, 2));
   }
 
-  // createPolygon() {
-  //   this.geometry = new THREE.BufferGeometry();
-  //   const vertices = [];
-  //   const verticesTemp = [];
-  //   const normals = [];
-  //   const colors = [];
-  //   const indices = [];
-  //   const part = this.radius/this.stacks;
-
-  //   // Calculate vertices and colors
-  //   for (let i = 0; i < this.stacks; i++) {
-  //     const theta = (i / this.stacks) * Math.PI;
-  //     for (let j = 0; j < this.slices; j++) {
-  //       const phi = (j / this.slices) * 2 * Math.PI;
-
-  //       const x = (i*part) * Math.sin(theta) * Math.cos(phi);
-  //       const y = (i*part) * Math.sin(theta) * Math.sin(phi);
-  //       const z = 0;
-
-  //       const normal1 = new THREE.Vector3(x, y, z).normalize();
-
-  //       verticesTemp.push(x, y, z);
-  //       normals.push(normal1.x, normal1.y, normal1.z);
-
-  //       if(i < this.stacks && j < this.slices){
-  //         const first = i * this.slices + j;
-  //         const second = i * this.slices + j + 1;
-  //         const third = (i + 1) * this.slices + j;
-  //         const fourth = (i + 1) * this.slices + j + 1;
-
-  //         indices.push(first, second, third);
-  //         indices.push(second, fourth, third);
-  //       }
-  //      }
-  //   }
-
-  //   //create vertex list from the indices
-  //   for(let i = 0; i < indices.length; i++){
-  //     const index = indices[i];
-  //     vertices.push(verticesTemp[index * 3], verticesTemp[index * 3 + 1], verticesTemp[index * 3 + 2]);
-  //     let n_stack = Math.floor((index) / (this.slices));
-  //     console.log(n_stack, index);
-  //     const stackInterpolation = n_stack / this.stacks;
-  //     const interpolatedColor = new THREE.Color().lerpColors(this.color_c, this.color_p, stackInterpolation);
-  //     colors.push(interpolatedColor.r, interpolatedColor.g, interpolatedColor.b);
-  //   }
-
-
-
-  //   console.log(vertices);
-  //   this.geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-  //   this.geometry.setAttribute('normal', new THREE.Float32BufferAttribute(normals, 3));
-  //   this.geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-  // }
-
-  addMaterial(material1, castshadow, receiveshadows){
-    const material = new THREE.MeshBasicMaterial({
-      color: 0xffffff,
-      vertexColors: true, // Use THREE.VertexColors enum value
-      transparent: false
-  });
-    let object = new THREE.Mesh(this.geometry, material);
-    if(castshadow){
+  addMaterial(material1, castshadow, receiveshadows) {
+    material1.vertexColors = true;
+    material1.setRepeat(this.radius, this.radius);
+    let object = new THREE.Mesh(this.geometry, material1);
+    if (castshadow) {
       object.castShadow = true;
     }
-    if(receiveshadows){
+    if (receiveshadows) {
       object.receiveShadow = true;
     }
     return object
