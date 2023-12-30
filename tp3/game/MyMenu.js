d470fdbc130fa91a6c42faa37c310c31d3378ee0
inescardos <ines.caardoso@gmail.com>
2023-12-30 23:03:45 +0000

    changing correctly the menu screens

--- tp3/game/MyMenu.js
+++ tp3/game/MyMenu.js
@@ -11,7 +11,31 @@ class MyMenu {
         this.app = app;
         this.initMenuEnvironment();
         this.buildStartPage();
+
+        document.addEventListener('mousedown', (event) => {
+            this.onDocumentMouseDown(event);
+        });
     }
+
+    onDocumentMouseDown(event) {
+        // Calculate mouse coordinates in normalized device coordinates
+        const mouse = new THREE.Vector2();
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        // Create a raycaster
+        const raycaster = new THREE.Raycaster();
+        raycaster.setFromCamera(mouse, this.app.activeCamera);
+
+        // Check for intersections with the button
+        const intersects = raycaster.intersectObject(this.startButton);
+
+        if (intersects.length > 0) {
+            if( intersects[0].object === this.startButton)
+                this.playerNamePage();
+        }
+    }
+
 
     initMenuEnvironment() {
         // Existing code for creating the first skybox
@@ -37,51 +61,53 @@ class MyMenu {
         // Create a box (cube) for the start button
         const boxGeometry = new THREE.BoxGeometry(10, 3, 0.5); // Adjust the size as needed
         const boxMaterial = new THREE.MeshBasicMaterial({ color: 0xFFBCF2 });
-        const startButton = new THREE.Mesh(boxGeometry, boxMaterial);
-        startButton.position.set(-1970, 249, -2490);
+        this.startButton = new THREE.Mesh(boxGeometry, boxMaterial);
+        this.startButton.position.set(-1970, 249, -2490);
 
         const startWord = this.myFont.getWord("START"); // Assuming getWord is a method to create a 3D text object
-        startWord.position.set(startButton.position.x + 3, startButton.position.y, startButton.position.z - 0.3);
+        startWord.position.set(this.startButton.position.x + 3, this.startButton.position.y, this.startButton.position.z - 0.3);
         startWord.rotation.y = Math.PI;
         startWord.scale.set(1.5, 1.5, 1.5); // Use set method to set scale
 
         const feupName = this.myFont.getWord("FEUP"); // Assuming getWord is a method to create a 3D text object
-        feupName.position.set(startButton.position.x + 0.5, startButton.position.y + 7, startButton.position.z);
+        feupName.position.set(this.startButton.position.x + 0.5, this.startButton.position.y + 7, this.startButton.position.z);
         feupName.rotation.y = Math.PI;
 
         // Name of the game
         const gameName = this.myFont.getWord("SUGAR RUSH"); // Assuming getWord is a method to create a 3D text object
-        gameName.position.set(startButton.position.x + 8, startButton.position.y + 5, startButton.position.z);
+        gameName.position.set(this.startButton.position.x + 8, this.startButton.position.y + 5, this.startButton.position.z);
         gameName.rotation.y = Math.PI;
         gameName.scale.set(2, 2, 2); // Use set method to set scale
 
         const studentsName = this.myFont.getWord("Made by the students:"); // Assuming getWord is a method to create a 3D text object
-        studentsName.position.set(startButton.position.x + 8, startButton.position.y - 5, startButton.position.z);
+        studentsName.position.set(this.startButton.position.x + 8, this.startButton.position.y - 5, this.startButton.position.z);
         studentsName.rotation.y = Math.PI;
         studentsName.scale.set(0.8, 0.8, 0.8);
 
         const inesName = this.myFont.getWord("Ines Cardoso up202005435"); // Assuming getWord is a method to create a 3D text object
-        inesName.position.set(startButton.position.x + 9, startButton.position.y - 6, startButton.position.z);
+        inesName.position.set(this.startButton.position.x + 9, this.startButton.position.y - 6, this.startButton.position.z);
         inesName.rotation.y = Math.PI;
         inesName.scale.set(0.8, 0.8, 0.8);
 
         const joanaName = this.myFont.getWord("Joana Santos up202005435"); // Assuming getWord is a method to create a 3D text object
-        joanaName.position.set(startButton.position.x + 9, startButton.position.y - 7, startButton.position.z);
+        joanaName.position.set(this.startButton.position.x + 9, this.startButton.position.y - 7, this.startButton.position.z);
         joanaName.rotation.y = Math.PI;
         joanaName.scale.set(0.8, 0.8, 0.8);
 
         // Create a group to contain all the objects
         this.menuGroup = new THREE.Group();
-        this.menuGroup.add(feupName, gameName, studentsName, inesName, joanaName, startButton, startWord);
+        this.menuGroup.add(feupName, gameName, studentsName, inesName, joanaName, this.startButton, startWord);
         this.app.scene.add(this.menuGroup);
+
     }
 
 
     playerNamePage(){
-        //this.app.scene.remove(this.menuGroup);
+        console.log("oi");
+        this.app.scene.remove(this.menuGroup);
 
         const insertName = this.myFont.getWord("Insert Your Name"); // Assuming getWord is a method to create a 3D text object
-        insertName.position.set(startButton.position.x + 8, startButton.position.y + 5, startButton.position.z);
+        insertName.position.set(this.startButton.position.x + 8, this.startButton.position.y + 5, this.startButton.position.z);
         insertName.rotation.y = Math.PI;
 
         this.app.scene.add(insertName);
